0561773bd6ebf68ab5b72f8d5ff306e3840b09f7
lukeyh99 <126897926+lukeyh99@users.noreply.github.com>
2023-03-24 17:42:16 +0000

    Update InstrumentalTest.kt

--- app/src/androidTest/kotlin/com/example/moodtracker/InstrumentalTest.kt
+++ app/src/androidTest/kotlin/com/example/moodtracker/InstrumentalTest.kt
@@ -36,12 +36,34 @@ class InstrumentalTest {
         scenario.close()
     }
 
-   /* @Test
+    @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.moodtracker", appContext.packageName)
-    }*/
+    }
+    
+    @Test
+    fun testButtonClick() {
+        // Launch the activity
+        val scenario = ActivityScenario.launch(MainActivity::class.java)
+
+        // Check that the Home Fragment is displayed
+        Espresso.onView(ViewMatchers.withId(R.id.home))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        // Find the button and click it
+        Espresso.onView(ViewMatchers.withId(R.id.Continue))
+            .perform(ViewActions.click())
+
+        // Add a delay or an idle resource to allow the app to transition to the next fragment
+        /*Espresso.onView(ViewMatchers.isRoot())
+            .perform(ViewActions.waiting(1000))*/
+
+        // Use a matcher to check if the correct fragment is displayed
+        Espresso.onView(ViewMatchers.withId(R.id.selectMood))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
 }
 
 
